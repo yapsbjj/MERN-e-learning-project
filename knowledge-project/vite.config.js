f81bf59b05e2e7ae5d51ef7e78773173130117eb
unknown <ya.ramdane01@gmail.com>
2025-06-09 17:49:15 +0200

    Fix: Vercel config + Vite base

--- knowledge-project/vite.config.js
+++ knowledge-project/vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // important pour Vercel
+  base: '/', // important pour Vercel
   plugins: [react(), tailwindcss()],
 })
