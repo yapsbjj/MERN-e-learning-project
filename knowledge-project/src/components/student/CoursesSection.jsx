898be4598a140b6d19adea66892b38ba54b402a4
unknown <ya.ramdane01@gmail.com>
2025-06-12 00:02:29 +0200

    add queries courseSection

--- knowledge-project/src/components/student/CoursesSection.jsx
+++ knowledge-project/src/components/student/CoursesSection.jsx
@@ -14,7 +14,8 @@ const CoursesSection = () => {
          De coder en passant par la musique ou apprendre a cuisiner comme un chef,
          notre pédagogie est affuter pour libérer tout votre potentiel !</p>
 
-        <div className='grid grid-cols-4 px-4 md:my-16 my-10 gap-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 my-16 gap-6 px-2 md:px-0'>
+
           {allCourses.slice(0,4).map((course, index)=> <CourseCard key={index} course={course}/>)}
         </div>
 
